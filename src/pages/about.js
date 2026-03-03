699523dc13d7ffe57a5ff5349007bf2544abfe42
dorian.wolff <dorian.wolff@epita.fr>
2026-03-03 16:32:42 +0100

    about us page started and changes implemented

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,34 @@ import { mountPage, sectionCard, textBlock, el } from '../ui.js';
 import { t } from '../i18n.js';
 
 export async function renderAbout(app) {
+  const curatorBlock = ({ name, imgSrc, alt }) =>
+    el('div', { class: 'curator' }, [
+      el('img', {
+        class: 'curator-img',
+        src: imgSrc,
+        alt,
+        loading: 'lazy',
+      }),
+      el('div', { class: 'curator-meta' }, [
+        el('div', { class: 'curator-name' }, [name]),
+        el('div', { class: 'curator-text' }, ['']),
+      ]),
+    ]);
+
+  const curators = [
+    curatorBlock({
+      name: 'Julia Tavares',
+      imgSrc: './Images/artists/Julia%20Tavares%20(curator).JPG',
+      alt: 'Julia Tavares',
+    }),
+    curatorBlock({
+      name: 'Amalia Mytilineou',
+      imgSrc: './Images/artists/Amalia%20Mytilineou%20(curator).jpeg',
+      alt: 'Amalia Mytilineou',
+    }),
+  ];
+  if (Math.random() < 0.5) curators.reverse();
+
   mountPage(
     app,
     {
@@ -10,21 +38,17 @@ export async function renderAbout(app) {
       showCredit: false,
     },
     [
-      sectionCard(t('about.curators_title'), [
-        el('div', { class: 'curators' }, [
-          el('div', { class: 'curators-media' }, [
-            el('img', {
-              class: 'curators-img',
-              src: './Images/artists/Julia%20Tavares%20(curator).JPG',
-              alt: 'Julia Tavares',
-              loading: 'lazy',
-            }),
-          ]),
-          el('div', { class: 'curators-names' }, ['Julia Tavares', ' ', '&', ' ', 'Amalia Mytilineou']),
+      el('div', { class: 'about-grid' }, [
+        el('div', { class: 'about-col about-col--exhibition' }, [
+          sectionCard('', [textBlock('')]),
         ]),
-      ]),
-      sectionCard(t('section.about'), [
-        textBlock(''),
+        el('div', { class: 'about-col about-col--curators' }, [
+          sectionCard(t('about.curators_title'), [
+            el('div', { class: 'curators' }, [
+              ...curators,
+            ]),
+          ]),
+        ]),
       ]),
     ]
   );
